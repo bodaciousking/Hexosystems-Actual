7ba8325573e7f2da553e5866ac4ba8b4c655373a
Eriki Enos <Erikjenos@hotmail.com>
2021-01-21 14:40:29 -0800

    Added Lan HUD/Connection

---
+++ Assets/Scripts/Main/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+namespace Mirror.Example.Pong
+{
+    
+
+    public class Player : NetworkBehaviour
+    {
+
+        //public int energy = 0;
+        //other player stats
+
+
+
+
+
+
+        // Update is called once per frame
+        void Update()
+        {
+
+            if (isLocalPlayer)
+            {
+                //checks if this player is the owner 
+            }
+
+        }
+    }
+
+}
